5219293510bd98ed48d7124cff7b4ad829c295f4
Brahan Quispe <brahan.quispe@tecsup.edu.pe>
2026-05-07 10:16:58 -0500

    Agregamos package.json

---
+++ index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const http= require('http');
+
+function requestListener(req, res){
+    console.log('Bienvenido al curso')
+}
+
+const server=http.createServer(requestController)
+
+const PORT= process.env.PORT
+
+server.listen(PORT, function(){
+    console.log(`Servidor escuchando en el puerto ${PORT}`)
+})
